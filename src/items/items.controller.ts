16af94685455dadbbccb0a4831b575dae6f84962
mahiyou <you.maede@gmail.com>
2023-11-27 06:51:16 -0300

    Load correct image for every url

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,6 +8,8 @@ import * as mime from 'mime';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Item } from './interfaces/item.interface';
+import puppeteer from 'puppeteer';
+import { v4 as uuid } from 'uuid';
 
 @Controller()
 export class ItemsController {
@@ -15,21 +17,31 @@ export class ItemsController {
 
     @Get("capture")
     @UsePipes(new ValidationPipe({ transform: true }))
-    async capture(@Query() query: CaptureRequest, @Res() res: Response, @Req() req: Request) {
+    async capture(@Query() query: CaptureRequest, @Res() res: Response) {
+        const imageName = uuid();
 
-        const image = path.resolve(__dirname, "..", "..", "public", "images", "w3.jpg");
+        this.getScreenShot(query, imageName).then(async () => {
+            const image = path.resolve(__dirname, "..", "..", "public", "images", `${imageName}.jpg`);
+            const webSiteUrl = query.url;
+            const newItem = new this.itemModel({ image: `${imageName}.jpg`, webSiteUrl })
+            newItem.save();
+            // const file = createReadStream(image);
+            const imageStat = await stat(image);
+            res.set({
+                'Content-Type': (mime as any).getType(image),
+                'Content-Length': imageStat.size,
+            });
+            // file.pipe(res);
+        })
+    }
 
-        const webSiteUrl = query.url;
-        const newItem = new this.itemModel({ image:"mokasarat.jpg",webSiteUrl })
-        newItem.save();
-
-        const file = createReadStream(image);
-        const imageStat = await stat(image);
-        res.set({
-            'Content-Type': (mime as any).getType(image),
-            'Content-Length': imageStat.size,
-        });
-        file.pipe(res);
+    async getScreenShot(query, imageName) {
+        const browser = await puppeteer.launch({ product: 'firefox' });
+        const page = await browser.newPage();
+        console.log(query.url)
+        await page.goto(query.url);
+        await page.screenshot({ path: `public/images/${imageName}.jpg` });
+        await browser.close();
     }
 
     @Get("gallery")
@@ -39,7 +51,7 @@ export class ItemsController {
 
     @Get(':filename')
     findFile(@Param('filename') filename, @Res() res: Response) {
-        res.sendFile(filename, {root: './public/images'})
+        res.sendFile(filename, { root: './public/images' })
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   // app.use('/public', express.static(join(__dirname, '..', 'public')));
   await app.listen(3001);
 }
